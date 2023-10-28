d68cfb6b471416da6d565ba4b80b1eeb1836fe13
Olamilekan <ayomikunolamilekan@gmail.com>
2023-10-28 17:18:34 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './App.css'
+import Header from './header'
+import Body from './body'
+import Footer from './footer'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div>
+      <Header />
+      <Body />
+      {/* <Footer /> */}
+    </div>
   )
 }
 
